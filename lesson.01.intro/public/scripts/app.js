14e2d37bcf5b24dc5dbc365e97d4038c9b418353
Adam Fortuno <30395811+adamfortuno@users.noreply.github.com>
2020-02-24 12:44:10 -0500

    Created React Boiler Plate from Sec-1; Moving existing sec-1 to sec-3

--- lesson.01.intro/public/scripts/app.js
+++ lesson.01.intro/public/scripts/app.js
@@ -1,108 +1,15 @@
-"use strict";
+'use strict';
+"set strict";
 
-console.log("app.js is running.");
+console.log('The Indicision App is running.');
 
-// Where I define the target of my template/stuff
 var appRoot = document.getElementById('app');
-
-// Template One
-var app = {
-    title: "Sabrina's Big Day",
-    subtitle: "Adventure",
-    options: ['Suzie', 'Billy', 'Micky']
-};
-
-function getSubTitle(app) {
-    if ([null, undefined, ""].includes(app.subtitle) === false) {
-        return React.createElement(
-            "p",
-            null,
-            app.subtitle
-        );
-    }
-}
-
-function getOptions(app) {
-    if (app.options && app.options.length > 0) {
-        return React.createElement(
-            "ol",
-            null,
-            app.options.join()
-        );
-    }
-}
-
-var templateOne = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        "p",
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        "p",
-        null,
-        app.options && app.options.length > 0 ? "Here are your options" : "No options"
-    ),
-    getOptions(app)
+var now = new Date();
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App - ',
+  now.toISOString()
 );
 
-// Template Two
-var count = 0;
-
-var renderPage = function renderPage() {
-    var templateTwo = React.createElement(
-        "div",
-        null,
-        React.createElement(
-            "h1",
-            null,
-            "Count: ",
-            count
-        ),
-        React.createElement(
-            "button",
-            { id: "button-minus", className: "button", onClick: countPlus },
-            "Increment"
-        ),
-        React.createElement(
-            "button",
-            { id: "buttom-reset", className: "button", onClick: countReset },
-            "Reset"
-        ),
-        React.createElement(
-            "button",
-            { id: "button-minus", className: "button", onClick: countMinus },
-            "Decrement"
-        )
-    );
-
-    ReactDOM.render(templateTwo, appRoot);
-};
-
-var countReset = function countReset() {
-    count = 0;
-    renderPage();
-};
-
-var countMinus = function countMinus() {
-    var number = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
-
-    count--;
-    renderPage();
-};
-
-var countPlus = function countPlus() {
-    var number = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
-
-    count++;
-    renderPage();
-};
-
-renderPage();
+ReactDOM.render(template, appRoot);
--- lesson.01.intro/src/app.js
+++ lesson.01.intro/src/app.js
@@ -1,77 +1,9 @@
-"use strict";
+"set strict";
 
-console.log("app.js is running.");
+console.log('The Indicision App is running.');
 
-// Where I define the target of my template/stuff
 const appRoot = document.getElementById('app');
+const now = new Date();
+const template = (<h1>Indecision App - {now.toISOString()}</h1>);
 
-// Template One
-const app = {
-    title: "Sabrina's Big Day",
-    subtitle: "Adventure",
-    options: ['Suzie', 'Billy', 'Micky']
-};
-
-function getSubTitle(app) {
-    if ( [null, undefined, ""].includes(app.subtitle) === false ) {
-        return <p>{app.subtitle}</p>;
-    }
-}
-
-function getOptions(app) {
-    if (app.options && app.options.length > 0) {
-        return (
-            <ol>
-                { app.options.join() }
-            </ol>
-        );
-    }
-}
-
-const templateOne = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{ (app.options && app.options.length > 0) ? "Here are your options" : "No options" }</p>
-        { getOptions(app) }
-    </div>
-);
-
-// Template Two
-let count = 0;
-
-const renderPage = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button id="button-minus" className="button" onClick={countPlus}>
-                Increment
-            </button>
-            <button id="buttom-reset" className="button" onClick={countReset}>
-                Reset
-            </button>
-            <button id="button-minus" className="button" onClick={countMinus}>
-                Decrement
-            </button>
-        </div>
-    );
-    
-    ReactDOM.render(templateTwo, appRoot);
-};
-
-const countReset = () => {
-    count = 0;
-    renderPage();
-};
-
-const countMinus = (number = 1) => {
-    count--;
-    renderPage();
-};
-
-const countPlus = (number = 1) => {
-    count++;
-    renderPage();
-};
-
-renderPage();
+ReactDOM.render(template, appRoot);
